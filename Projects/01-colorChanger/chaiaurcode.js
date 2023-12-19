62056257c0f27c35e3aaecee8956326ba5fcee4e
PrathamShamra1111 <sharmapratham223@gmail.com>
2023-12-19 23:51:08 +0530

    new_projects

--- Projects/01-colorChanger/chaiaurcode.js
+++ Projects/01-colorChanger/chaiaurcode.js
@@ -7,17 +7,28 @@ buttons.forEach(function (button) {
   button.addEventListener('click', function (e) {
     console.log(e);
     console.log(e.target);
+    const greet=document.createElement('h1');
+    greet.innerText = " ";
+    greet.style.textAlign = 'center'; // Center the text
+    greet.style.marginTop = '50vh';
+      body.appendChild(greet);
     if (e.target.id === 'grey') {
       body.style.backgroundColor = e.target.id;
+       document.querySelector('h1').innerText="Hello I am Grey"
     }
     if (e.target.id === 'white') {
       body.style.backgroundColor = e.target.id;
+     
+      document.querySelector('h1').innerText="Hello I am White"
     }
     if (e.target.id === 'blue') {
       body.style.backgroundColor = e.target.id;
+      document.querySelector('h1').innerText="Hello I am Blue"
     }
     if (e.target.id === 'yellow') {
       body.style.backgroundColor = e.target.id;
+   
+      document.querySelector('h1').innerText="Hello I am Yellow"
     }
     
   });
